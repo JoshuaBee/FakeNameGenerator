38d6a6988af0408760688e36638c89d670023675
Joshua Whiting <J.Whiting-2018@hull.ac.uk>
2019-04-30 17:38:12 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1401,7 +1401,6 @@ var names = [
 ];
 
 document.addEventListener('DOMContentLoaded', function(event){
-   
 	// Apply generator to the title
 	document.getElementById("titleOutput").innerHTML = generator(document.getElementById("title").innerHTML);
 	
@@ -1411,42 +1410,6 @@ document.addEventListener('DOMContentLoaded', function(event){
 	document.getElementById("name").focus();
 		
 	generateFromTextBox();
-	// Load the profanity list
-	/*var request = new XMLHttpRequest();
-	request.open("GET", "data/profanity_list.json", false);
-	request.send(null)
-	request.onreadystatechange = function() {
-		if ( request.readyState === 4 && request.status === 200 ) {
-			profanity = JSON.parse(request.response);
-			console.log(profanity[0]);
-		}
-	}
-	var profanity_request = new XMLHttpRequest();
-    profanity_request.addEventListener("load", function() {
-        profanity = JSON.parse(profanity_request.response);
-   
-		// Apply generator to the title
-		document.getElementById("titleOutput").innerHTML = generator(document.getElementById("title").innerHTML);
-		
-		
-    }, false);
-    profanity_request.open("GET", "data/profanity_list.json", false);
-    profanity_request.send();
-	
-	// Check that both profanity and names have been loaded
-	var names_request = new XMLHttpRequest();
-	names_request.addEventListener("load", function() {
-		names = JSON.parse(names_request.response);
-	
-		// Automatically fill the name textbox, and generate the fake
-		var name = random(names);
-		document.getElementById("name").value = name;
-		document.getElementById("name").focus(); 
-		
-		generateFromTextBox();
-	}, false);
-	names_request.open("GET", "data/names.json", false);
-	names_request.send();*/
 });
 
 function generator(s) {
