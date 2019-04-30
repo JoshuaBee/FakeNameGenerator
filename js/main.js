1ec942d39f3011f874c69ae10d864a5b4747a8ce
Joshua Whiting <J.Whiting-2018@hull.ac.uk>
2019-04-30 21:18:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,29 +2,46 @@
 const input_vowels = ["a", "e", "i", "o", "u", "y"];
 const output_vowels = ["a", "e", "i", "o", "u"];
 
-var names = [
-	"Rami Malek",
-	"Olivia Colman",
-	"Gary Oldman",
-	"Frances McDormand",
-	"Casey Affleck",
-	"Emma Stone",
-	"Leonardo DiCaprio",
-	"Brie Larson",
-	"Eddie Redmayne",
-	"Julianne Moore"
-];
+var profanity;
+var names;
 
 document.addEventListener('DOMContentLoaded', function(event){
-	// Apply generator to the title
-	document.getElementById("titleOutput").innerHTML = generator(document.getElementById("title").innerHTML);
-	
-	// Automatically fill the name textbox, and generate the fake
-	var name = random(names);
-	document.getElementById("name").value = name;
-	document.getElementById("name").focus();
+	// Load the profanity list
+	/*var request = new XMLHttpRequest();
+	request.open("GET", "data/profanity_list.json", false);
+	request.send(null)
+	request.onreadystatechange = function() {
+		if ( request.readyState === 4 && request.status === 200 ) {
+			var profanity = JSON.parse(request.responseText);
+			console.log(profanity[0]);
+		}
+	}*/
+	var profanity_request = new XMLHttpRequest();
+    profanity_request.addEventListener("load", function() {
+        profanity = JSON.parse(profanity_request.response);
+   
+		// Apply generator to the title
+		document.getElementById("titleOutput").innerHTML = generator(document.getElementById("title").innerHTML);
 		
-	generateFromTextBox();
+		var names_request = new XMLHttpRequest();
+		names_request.addEventListener("load", function() {
+			names = JSON.parse(names_request.response);
+		
+			// Automatically fill the name textbox, and generate the fake
+			var name = random(names);
+			document.getElementById("name").value = name;
+			document.getElementById("name").focus(); 
+			
+			generateFromTextBox();
+		}, false);
+		names_request.open("GET", "data/names.json", false);
+		names_request.send();
+    }, false);
+    profanity_request.open("GET", "data/profanity_list.json", false);
+    profanity_request.send();
+	
+	// Check that both profanity and names have been loaded
+	
 });
 
 function generator(s) {
@@ -56,13 +73,33 @@ function generator(s) {
 		r += c;
 	}
 	
+	console.log(r)
+	
+	// Check for profanity
+	if (check_profanity(r)) {
+		r = generator(s);
+	}
+	
 	return r;
 }
 
 function random(n) {
-	return n[Math.floor(Math.random() * (n))];
+	return n[Math.floor(Math.random() * (n.length))];
 }
 
 function generateFromTextBox() {
 	document.getElementById("nameOutput").innerHTML = generator(document.getElementById("name").value);
+}
+
+function check_profanity(s) {
+	c = 0;
+	for(var i = 0; i < profanity.length; i++) {
+		for(var j = 0; j < s.length; j++) {
+			if(profanity[i] == s.substring(j, j + profanity[i].length).toLowerCase()) {
+				c++;
+			}
+		}
+	}
+	
+	return c > 0;
 }
